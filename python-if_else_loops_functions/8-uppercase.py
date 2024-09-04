bd8336eeaf2f63a1f4b3742872ccfc398488ca1f
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-04 18:22:08 +0000

    Making a function to translate from lowercase to uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+def uppercase(str):
+    for i in range(0, len(str)):
+        if ord(str[i]) >= 97:
+            p = ord(str[i]) - 32
+            a = chr(p)
+        elif ord(str[i]) < 97:
+            a = str[i]
+        if i == len(str) - 1:
+            print("{}" .format(a))
+        else:
+            print("{}" .format(a), end="")
