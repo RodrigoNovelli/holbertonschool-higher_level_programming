a17a0a9a14098f3a21aa7a3591f3f1dc01f08983
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-04 23:38:52 +0000

    Solving pycodestyle

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -5,9 +5,9 @@ def print_last_digit(number):
         return ("")
     else:
         strn = str(number)
-        for i in range (0, len(strn)):
+        for i in range(0, len(strn)):
             if i == len(strn) - 1:
                 print("{}" .format(strn[i]), end="")
             else:
                 continue
-        return(strn[i])
+        return (strn[i])
