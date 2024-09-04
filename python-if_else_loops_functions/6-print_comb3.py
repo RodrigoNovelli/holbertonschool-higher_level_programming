57a2f2c101bfc6d5d5d480c4a05b6bc96e898f53
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-04 17:03:48 +0000

    solving pycode

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for number in range(0, 10):
     for num in range(number + 1, 10):
         if num == 9 and number == 8:
             print("{0}{1}".format(number, num))
-        else: 
+        else:
             print("{0}{1}".format(number, num), end=", ")
