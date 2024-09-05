dbae95e95f5fef021fbc9c5b9126e9e5ec2f3e89
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-05 00:01:49 +0000

    Making a function that makes the power of b for argumet a

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pow(a, b):
+    j = 1
+    if b > 0:
+        for i in range(1, b + 1):
+            j = j * a
+    else:
+        for i in range(1, abs(b) + 1):
+            j = j / a
+    return (j)
