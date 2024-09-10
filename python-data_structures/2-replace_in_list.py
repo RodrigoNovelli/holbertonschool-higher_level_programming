d4ccf1668cf39eab19e2053fcf0b73a8769986d4
GiorginaSerron <giorginaserron02@gmail.com>
2024-09-10 00:20:41 -0300

    xd

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return(my_list)
-    elif idx < (len(my_list) - 1):
-        return(my_list)
-    else:
         my_list.insert(idx, element)
         return(my_list)
