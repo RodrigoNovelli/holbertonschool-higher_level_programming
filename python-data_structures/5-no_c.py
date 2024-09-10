147c88ee50c8dba8f69f1e85e79b25c26bb00752
GiorginaSerron <giorginaserron02@gmail.com>
2024-09-10 01:02:37 -0300

    5. Can you C me now?

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for i in my_string:
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            list.remove(my_string[i])
+        else:
+            continue
+    return (my_string)
