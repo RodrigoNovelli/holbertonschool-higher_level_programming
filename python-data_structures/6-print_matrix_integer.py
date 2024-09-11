9f73d871acbed728220563d070ad0bc088451efd
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-11 00:11:30 +0000

    Using an nested loop to print a list of lists

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix) -1):
-        print("{:d}" .format(matrix[i]))
+    if matrix == [[]]:
+        print()
+    for i in range(0, len(matrix)):
+        for x in range(0, len(matrix[i])):
+            if x != len(matrix[i]) - 1:
+                print("{:d}" .format(matrix[i][x]), end=" ")
+            else:
+                print("{:d}" .format(matrix[i][x])
