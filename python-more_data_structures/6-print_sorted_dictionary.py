6efdf792ae3811237d38b3a9fe5c15bccdd69477
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-11 19:36:30 +0000

    solving format of printing

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     new_list = sorted(a_dictionary)
     for i in new_list:
-        print("{} : {}" .format(i, a_dictionary[i]))
+        print("{}: {}" .format(i, a_dictionary[i]))
