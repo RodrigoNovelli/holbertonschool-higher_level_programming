adfed3778b7f38bd3b76f9f4ee0ad5efd3441f9d
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-11 14:11:18 +0000

    Solving empty list case

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_number = my_list[0]
     if my_list == []:
         return ("None")
     else:
+        max_number = my_list[0]
         for i in my_list:
             if i > max_number:
                 max_number = i
