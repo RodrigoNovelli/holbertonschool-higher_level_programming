1a40720dad5ca87ecb67c905e9b0d9da9c844c74
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-11 14:53:01 +0000

    solving border cases

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    a = my_list[idx]
-    my_list.remove(a)
-    return (my_list)
+    if idx > len(my_list) or idx < 0:
+        return(my_list)
+    else:
+        a = my_list[idx]
+        my_list.remove(a)
+        return (my_list)
