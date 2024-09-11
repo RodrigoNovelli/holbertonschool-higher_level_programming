2b2a27555ed61eb8a243b1220722d92f03936774
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-11 14:06:33 +0000

    Making a function that returns the max number of a secuence of integers

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_number = my_list[0]
+    if my_list == "":
+        return ("None")
+    else:
+        for i in my_list:
+            if i > max_number:
+                max_number = i
+        return (max_number)
