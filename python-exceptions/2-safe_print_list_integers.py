d8fd3861ba0134c9978bb0dc5be5a2e6840e46aa
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-13 16:34:15 +0000

    Handeling none case

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
+    if my_list is None:
+        my_list = ""
     for i in range(0, x):
         try:
             print("{:d}" .format(my_list[i]), end="")
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             continue
         else:
             counter += 1
