6a92a695848ab7e1c6d8493a54bb93dc885c2981
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-13 16:49:05 +0000

    Prints None and returns 0 in None case

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     if my_list is None or x is None:
-        return (counter)
+        print("None")
+        return (0)
     for i in range(0, x):
         try:
-            element = my_list[i]
-            if isinstance(element, int):
-                print("{:d}" .format(my_list[i]), end="")
-                counter += 1
+            print("{:d}" .format(my_list[i]), end="")
+            counter += 1
         except ValueError as e:
             continue
     print()
