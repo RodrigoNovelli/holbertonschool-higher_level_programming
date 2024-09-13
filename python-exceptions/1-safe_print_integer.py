827a24c391003e3b54971b1b847f361cf7848229
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-13 15:18:22 +0000

    Trying chanmging the tpe of exception

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}" .format(value))
-    except Exception as e:
+    except (ValueError, TypeError):
         return (False)
     else:
-        print("{:d}" .format(value))
         return (True)
