0ce6e890b0e6b770cbc2df0e2a1d528520af9a7c
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-13 16:46:54 +0000

    None type prints 0 counter

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
-    if my_list is None:
-        my_list = ""
+    if my_list is None or x is None:
+        return (counter)
     for i in range(0, x):
         try:
             element = my_list[i]
