59fdb9095b2949937543604de3193f2fd481f632
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-14 02:02:09 +0000

    Taking the spáce between "" and .format

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,9 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
         return (0)
     for i in range(0, x):
         try:
-            print("{:d}" .format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             counter += 1
         except (ValueError, TypeError):
-            pass
+            continue
     print()
     return (counter)
