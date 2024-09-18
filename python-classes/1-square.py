ab027ca96859e62a3ae2e68f227cfa6b8d452f1c
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-18 14:32:17 +0000

    Making a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This is a mudle that defines a class
+"""
+
+
+class Square:
+    """
+    This is a class that defines a square
+    """
+    
+    def __init__(self, size):
+        self.__size = size
