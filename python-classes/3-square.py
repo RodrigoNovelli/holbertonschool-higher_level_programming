f47a2e87a83f748ae35d69a3c4d94d9172d45acb
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-18 18:21:09 +0000

    Solving null problem

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
     """
     This is a class that defines a square
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
