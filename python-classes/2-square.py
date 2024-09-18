b2d6bb818bebcb2d06948ed31f8f3637acb4bf66
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-18 18:19:30 +0000

    Handeling size errrors and adding a module that returns the area of the square

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a mudle that defines a class
+"""
+
+
+class Square:
+    """
+    This is a class that defines a square
+    """
+    def __init__(self, size):
+        self.__size = size
---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is a mudle that defines a class
+"""
+
+
+class Square:
+    """
+    This is a class that defines a square
+    """
+    def __init__(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    
+    def area(self):
+        return (self.__size * self.__size)
