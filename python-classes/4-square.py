48d710cc6903f5b35f2d49624c13c699646b77aa
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-18 18:34:14 +0000

    Using getter and setter to change a private date

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,6 +18,17 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-            
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def setter(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
     def area(self):
         return (self.__size * self.__siz)
