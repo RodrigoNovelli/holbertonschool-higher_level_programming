ea9ea4d66e89213e72bebb2ddcdfccacb236eaa1
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-25 14:06:30 +0000

    Making a function that checks if a class has istance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    
+    """
+    Check if the object is exactly an instance of the specified class.
+
+    This function returns True if the object is an instance of the specified 
+    class and False otherwise. It does not consider inheritance, meaning 
+    it will only return True if the object is created from the exact class 
+    provided.
+    """
+
+    return (type(obj) is a_class)
