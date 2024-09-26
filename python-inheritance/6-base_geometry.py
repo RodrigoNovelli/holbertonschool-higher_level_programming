3168eeb80dfb080f2052a7ba38dddef182fd358e
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-26 00:29:45 +0000

    Making a new method area

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""
+This is a module that defines a class
+"""
+
+
+class BaseGeometry:
+
+    """
+    This is an empty class
+    """
+    def area(self):
+
+        """
+        Method to calculate area
+        """
+
+        raise Exception("area() is not implemented")
