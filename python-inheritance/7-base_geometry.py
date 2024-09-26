93ca4ce32f01d7be75bd50093dc42cf70dc61551
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-26 00:38:13 +0000

    New method that checks int

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+"""
+This is a module that defines a class
+"""
+
+
+class BaseGeometry:
+
+    """
+    This is an empty class
+    """
+    def area(self):
+
+        """
+        Method to calculate area
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        """
+        Method to validate if a number is an interger
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        elif value <= 0:
+            raise ValueError("<name> must be greater than 0")
