18931740ffd4d5f682c445b597b038cd27e3a5b2
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-27 20:33:56 +0000

    Changing module documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
+
+
+"""
+This module defines a Rectangle class that inherits from BaseGeometry.
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+
+    """
+    Rectangle class that inherits from BaseGeometry.
+    """
+
     def __init__(self, width, height):
+
+        """
+        Initializes a Rectangle object.
+
+        Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        """
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
