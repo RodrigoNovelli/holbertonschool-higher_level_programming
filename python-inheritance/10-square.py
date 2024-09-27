b2723b5ec020bc41ab40922ceefde0f263b7b93b
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-27 20:57:08 +0000

    adding validator for size

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -16,5 +16,5 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        self.integer_validator("size", height)
+        self.integer_validator("size", size)
         super().__init__(size, size)
