e9640ea2107070403fcb07a74e5d2b4da607e327
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-09-27 20:24:44 +0000

    Solving identation problems

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 BaseGeoametry = __import__('7-base_geometry').BaseGeometry
-    class Rectangle(BaseGeometry):
-        
-        def __init__(self, width, height):
-            self.integer_validator("width", width)
-            self.integer_validator("height", height)
-            self.__width = width
-            self.__height = height
+
+
+class Rectangle(BaseGeometry):
+
+    """
+    Rectangle class that inherits from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+
+        """
+        Initializes a Rectangle object.
+
+        Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
