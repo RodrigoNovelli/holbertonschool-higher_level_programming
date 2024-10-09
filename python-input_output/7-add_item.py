e619fccb0ae01043c6df8e60f1005a2879d6820b
Rodrigo <Rodrigonovelli21@hotmail.com>
2024-10-09 11:39:16 -0300

    module

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+Este script carga una lista desde un archivo JSON, agrega nuevos elementos
+pasados como argumentos de la línea de comandos y guarda la lista de nuevo
+en el archivo JSON.
+"""
+
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
