5bc60b868c8e28966936162a3ce6b21615412b37
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-10-17 19:34:57 -0300

    changing argument

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -10,7 +10,7 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     with open(filename, 'w') as file:
-        json.dump(data, filename)
+        json.dump(data, file)
 
 def load_and_deserialize(filename):
     with open (filename, 'r') as file:
