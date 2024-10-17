3b9e8837458a444cd29ac8a71e45d69a44f1dac7
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2024-10-17 17:50:11 -0300

    Changing //data

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -22,7 +22,11 @@ class Server(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.data = {"name": "Jhon", "age": 30, "city": "New York"}
+            self.data = {
+                "name": "Jhon",
+                "age": 30,
+                "city": "New York"
+                }
             self.wfile.write(json.dumps(self.data).encode('utf-8'))
         elif self.path == '/status':
             self.send_response(200)
