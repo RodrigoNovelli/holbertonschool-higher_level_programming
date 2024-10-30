c58b0b144adf98cef52933333bb927edc87ff658
Rodrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-10-30 13:20:57 -0300

    tRYING AGAIN

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table if it does not exists
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT PRIMARY KEY,
-    name VARCHAR(256),
+    id INT,
+    name VARCHAR(256)
 );
