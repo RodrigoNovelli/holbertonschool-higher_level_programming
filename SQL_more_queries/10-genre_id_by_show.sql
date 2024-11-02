a78c4c8e54acd9d2edbe2e0b8fbdf6047289b223
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-11-02 18:24:38 -0300

    10. Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,11 @@
+-- Making a new script
+SELECT 
+tv_shows.title,
+tv_show_generes.genre_id
+FROM
+tv_shows, tv_show_generes
+WHERE
+tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY
+tv_shows.title ASC,
+tv_show_genres.genre_id ASC;
