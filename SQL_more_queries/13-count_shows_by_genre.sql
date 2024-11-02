502ae14594841125235f16275ff4a83a63d16359
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-11-02 19:50:42 -0300

    changing names for genre with AS

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Making a script that shows all elements in a list and counts the elements from other table that matches with them
-SELECT tv_genres.name,
+SELECT tv_genres.name AS genre,
 COUNT(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
