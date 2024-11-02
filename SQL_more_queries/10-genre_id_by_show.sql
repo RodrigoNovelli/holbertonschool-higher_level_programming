eb9142b0db4db33086e51128f72afd64b541c334
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-11-02 18:56:37 -0300

    tying another way

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,11 +1,8 @@
--- Making a new script
-SELECT 
-tv_show.title,
+-- Script tath sows all matched elements between to tables
+SELECT
+tv_shows.title,
 tv_show_genres.genre_id
-FROM
-tv_shows
-JOIN
-tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY
-tv_shows.title ASC,
-tv_show_genres.genre_id ASC;
+FROM tv_shows AS tv_shows
+INNER JOIN tv_show_genres AS tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
