52e4d9a86f0554a383546cff38b65381090dfba2
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-11-03 20:50:07 -0300

    deleting the comma in port

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ def main():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(host='localhost', port='3306',
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=username, password=password, db=database)
     cursor = db.cursor()
     cursor.execute(
