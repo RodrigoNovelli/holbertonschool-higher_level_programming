0877e8c74c7432f1e8428b54a840c3a982645d00
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2024-11-03 20:09:33 -0300

    making a script that shows all the states in a data base (elements of a table) in python

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-'''
+"""
 Making a script that shows all the states in a database.
-'''
-
-
+"""
 import MySQLdb
 from sys import argv
 
-params = {
-    'host': 'localhost',
-    'port': 3306,
-    'user': argv[1],
-    'passwd': argv[2],
-    'db': argv[3],
-}
+
+def main():
+    """
+    This script connects to a MySQL database and
+    returns all the elements from states table
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, password=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(**params)
-    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-    for i in range(num_rows):
-        print(cur.fetchone())
+    main()
