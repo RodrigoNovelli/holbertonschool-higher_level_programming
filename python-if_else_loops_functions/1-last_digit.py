5b1b5f601295e21f48f52e1d1ab2502dba03a039
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2025-01-15 12:51:30 -0300

    converting the last digit to the sign that correspond

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,7 @@ if number > 0:
     last_digit = number % 10
 else:
     last_digit = number * -1  % 10
+    last_digit = last_digit * -1
 str = "Last digit of {} is {} and".format(number, last_digit)
 if last_digit > 5:
     print(str + " is greater than 5")
