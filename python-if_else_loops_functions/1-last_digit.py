55a922c3e417ad67958ab2cdac9af32985a41d25
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2025-01-15 12:47:42 -0300

    Making a script that prints the last digit of a random number and checks if its greater or less than 5,also if its 0

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+str = "Last digit of {} is {} and".format(number, last_digit)
+if last_digit > 5:
+    print(str + " is greater than 5")
+elif last_digit < 6 and last_digit != 0:
+    print(str + " is less than 6 and not 0")
+else:
+    print(str + " is 0")
