677687123437a9029872974a22ebce68864209f7
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-01-22 12:10:10 -0300

    changing the range of the loop

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     num_max = 0
     if len(my_list) != 0:
-        for value in range(len(my_list) - 1):
+        for value in range(len(my_list)):
             if my_list[value] > num_max:
                 num_max = my_list[value]
             else:
