9a368c557e37ee2823a9779401edf271d4480de0
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-01-22 12:12:09 -0300

    Starting with the first number instead of 0, so if the list is negative the function still work

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    num_max = 0
     if len(my_list) != 0:
         for value in range(len(my_list)):
-            if my_list[value] > num_max:
+            if value == 0:
+                num_max = my_list[value]
+            elif my_list[value] > num_max:
                 num_max = my_list[value]
             else:
                 pass
