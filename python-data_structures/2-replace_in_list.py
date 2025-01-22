e441207b1814ab3104250c5ee5287718c64c2474
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-01-22 10:00:02 -0300

    changing the first line

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,4 @@
-#!/bin/usr/python3
-'''
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
@@ -9,12 +8,3 @@ def replace_in_list(my_list, idx, element):
         new_list = my_list.copy()
         new_list[idx] = element
         return new_list
-'''
-#!/bin/usr/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):  # Check if idx is out of bounds
-        return my_list
-    else:
-        new_list = my_list.copy()  # Create a copy of the list
-        new_list[idx] = element  # Replace the element
-        return new_list  # Return the modified list
