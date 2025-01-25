41f55632efa9b9d572ec3c987a7d8d019851d08d
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2025-01-25 18:15:45 -0300

    makingafunctionthat translates fromromannumbers to int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_map = {
+    'M' : 1000,
+    'D' : 500,
+    'C' : 100,
+    'L' : 50,
+    'X' : 10,
+    'V' : 5,
+    'I' : 1
+    }
+    int_value = 0
+    prev_value = 0
+    current_value = 0
+    for letter in reversed(roman_string):
+        current_value = roman_map[letter]
+        if current_value >= prev_value:
+            int_value += current_value
+        else:
+            int_value -= current_value
+        prev_value = current_value
+    return int_value
