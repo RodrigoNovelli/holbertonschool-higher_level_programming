a08bf97099e29806321d76121dff009cb20d8f58
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-02-02 23:32:27 -0300

    MAKING AREA METHOD

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This is a mudle that defines a class
+"""
+
+
+from typing import Any
+
+
+class Square:
+    """
+    This is a class that defines a square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            
+    def area(self):
+        return (self.__size * self.__siz)
