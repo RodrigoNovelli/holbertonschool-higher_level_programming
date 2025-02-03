58a6ca72018ad8be50a3f3ad6e825cd6e73ecb07
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-02-02 23:45:15 -0300

    adding new line

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,7 +7,7 @@ This is the "Square"  module.
 class Square:
     """A class that defines a square by size"""
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
