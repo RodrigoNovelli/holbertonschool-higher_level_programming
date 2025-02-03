e1a8cf96f4814a9334bcdfc5bb0b31fbbee0d0f1
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-02-02 23:34:13 -0300

    SOLVING PROBLEMS

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,14 +3,10 @@
 This is a mudle that defines a class
 """
 
-
-from typing import Any
-
-
 class Square:
-    """
+    '''
     This is a class that defines a square
-    """
+    '''
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("")
