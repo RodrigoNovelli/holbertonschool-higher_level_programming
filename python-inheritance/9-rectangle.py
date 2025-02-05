bdde045a1e52d23bad1cd3fba8b5a3d1bf40b41d
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-02-05 14:23:30 -0300

    CHanging a misspelling in line 19

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,10 +16,10 @@ class Rectangle(BaseGeometry):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
-        self.__hieght = height
+        self.__height = height
 
     def area(self):
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
