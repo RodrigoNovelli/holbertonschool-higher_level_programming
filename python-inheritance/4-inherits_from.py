428dbcf7908b9b03d0f7c2bb9baa5471e3f96316
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-02-05 13:18:34 -0300

    Makinga function that checks if its a chilf of a specificed class, directly or indirectly

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+In this module we are checking if the obj is 
+directly or indiretly a child form a class
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+    This function checks
+    '''
+    return isinstance(obj, a_class) and type(obj) is a_class
