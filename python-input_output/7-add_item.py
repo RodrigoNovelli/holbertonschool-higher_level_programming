a32b755a67ed273d03b374660e5de035f7442fa5
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-02-13 12:07:37 -0300

    adding try and except

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,7 +13,10 @@ from 5-save_to_json_file import save_to_json_file
 
 
 filename = "add_item.json"
-content = load_from_json_file(filename)
+try:
+    content = load_from_json_file(filename)
+except:
+    content = []
 for i in range(1, len(argv)):
     content.append(argv[i])
 save_to_json_file(content, filename)
