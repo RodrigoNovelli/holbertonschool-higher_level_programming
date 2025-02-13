cb321fb613618eced244f12bce02b602010df386
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-02-13 11:15:20 -0300

    Making a function that writes a file and returns the amount of characters

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This module is used to create a function
+that writes a line in a file
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    This function creates a file and 
+    writes it
+    '''
+    with open(filename, 'w', encoding='utf-8') as file:
+        char = file.write(text)
+        return char
