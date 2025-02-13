a11f063812c271e7b5610419db415928a1f38549
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-02-13 12:05:46 -0300

    making a script that loads the arguments passed to a json file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+This module is to make a function that
+adds the arguments passed to a list in pyhton
+and then making a file to store it as a
+json file.
+'''
+
+
+import sys
+from 6-load_from_json_file import load_from_json_file
+from 5-save_to_json_file import save_to_json_file
+
+
+filename = "add_item.json"
+content = load_from_json_file(filename)
+for i in range(1, len(argv)):
+    content.append(argv[i])
+save_to_json_file(content, filename)
