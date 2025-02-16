0ffd97be24122fec652268d33209bd14259c5401
RodrigoNovelli <Rodrigonovelli21@hotmail.com>
2025-02-16 19:12:29 -0300

    json seriializatrin

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that serializates a Python
+dictionary to a JSON file
+"""
+
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    with open (filename, 'r') as file:
+        return json.load(file)
