4cf64fedf4bbe77bb4871030d1d74054b96e9680
Rodrigo Novelli <Rodrigonoelli21@hotmail.com>
2025-02-19 13:55:12 -0300

    Making a simple API

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+'''
+In this module we are creating a simple
+RESTful API with httpsserver module
+'''
+
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header("Content-type", "application/js")
+            self.end_headers()
+            self.data = {"name": "Jhon", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(self.data).encode('utf-8'))
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header("Content-type", "text/application/js")
+            self.end_headers()
+            self.info = {
+                "version": "1.0",
+                "description": "A simple API built wit http"
+            }
+            self.wfile.write(json.dumps(self.info).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+def run_server(server_class=HTTPServer, handler_class=Server, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Servidor corriendo en el puerto {port}...")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run_server()
