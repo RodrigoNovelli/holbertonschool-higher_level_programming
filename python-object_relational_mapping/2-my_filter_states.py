9daf2a4e24f1a742513ae7baf15d991656e1abad
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-03-25 23:22:32 -0300

    MAKING A SCRIPT THAT SEARCHES FOR A WORD

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+'''
+In this module we are making a script
+that searches for a word in a table
+of a given database
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    exe = "SELECT * FROM states WHERE name='{:s}'\
+        ORDER BY states.id".format(argv[4])
+    cur.execute(exe)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
