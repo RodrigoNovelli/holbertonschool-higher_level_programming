0430846f6dc9fc9fa49e27b2bd1e807bf00908cd
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-03-26 00:09:23 -0300

    puting () in fetchall

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     exe = "SELECT cities.id, cities.name, states.name FROM cities\
         JOIN states ON cities.state_id = states.id ORDER BY cities.id"
     cur.execute(exe)
-    rows = cur.fetchall
+    rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
