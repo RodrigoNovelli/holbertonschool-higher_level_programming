07dbf70f34f26a6f722c2ae1b95c41ed8e458e67
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-03-25 23:49:40 -0300

    adding argv in passwd

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,9 +12,12 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=[2], db=argv[3])
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     exe = "SELECT * FROM states WHERE name=%s ORDER BY states.id"
     rows = cur.execute(exe, (argv[4], ))
     for row in rows:
         print(cur.fetchone())
+    cur.close()
+    db.close()
+
