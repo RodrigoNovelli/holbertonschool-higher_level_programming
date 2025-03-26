34126068f3bbee718c4ec2351ee6a5d6782c7f1f
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-03-25 21:52:09 -0300

    changing a static function for a function that gets the arguments from argv

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,13 @@ In this module we are making a script that
 gets information from a database
 '''
 import MySQLdb
+from sys import argv
 
 
-def list_states(username, password, database):
-    '''
-    Function that lists allthe
-    states from the database
-    '''
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        password=password,
-        db=database
-    )
-
-    cur = conn.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cur.execute(query)
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
+if __name__ == "__name__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    for row in rows:
+        print(cur.fetchone())
