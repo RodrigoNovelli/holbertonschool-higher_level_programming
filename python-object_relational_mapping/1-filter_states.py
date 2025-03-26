f5f1e824652605b799000826b932d0477de821d0
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-03-25 22:28:51 -0300

    Making it case senitive

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,12 @@ import MySQLdb
 from sys import argv
 
 
+i = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY states.id"
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute(i)
     rows = cur.fetchall()
     for row in rows:
         print(row)
