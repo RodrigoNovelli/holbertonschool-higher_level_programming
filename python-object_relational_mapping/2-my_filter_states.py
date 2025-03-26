281970a3797d2e63be64321ec49ab60fa98befa6
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-03-25 23:36:33 -0300

    Making it case sensitive

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    exe = "SELECT * FROM states WHERE name='{:s}'\
+    exe = "SELECT * FROM states WHERE BINARY name='{:s}'\
         ORDER BY states.id".format(argv[4])
     cur.execute(exe)
     rows = cur.fetchall()
