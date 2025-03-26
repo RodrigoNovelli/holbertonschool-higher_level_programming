88c4fc0b49d845763bf84825b7fd7432b7d7ebe3
ROdrigo Novelli <Rodrigonovelli21@hotmail.com>
2025-03-26 00:05:35 -0300

    ex 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+'''
+This module is for making a script that
+list all the cities in a state
+'''
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    exe = "SELECT cities.id, cities.name, states.name FROM cities\
+        JOIN states ON cities.state_id = states.id ORDER BY cities.id"
+    cur.execute(exe)
+    rows = cur.fetchall
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
